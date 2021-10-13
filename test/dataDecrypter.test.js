0b25c17143c0e082147df92f713ca7c1bb04e47e
lvchengyu <lvchengyu@dm-ai.com>
2021-10-13 17:24:57 +0800

    #2 fix test

--- test/dataDecrypter.test.js
+++ test/dataDecrypter.test.js
@@ -7,21 +7,21 @@ const { dataDecrypter } = require('../src/data/dataDecrypter')
 
 describe('dataDecrypter', () => {
     it('data/template.dat', async () => {
-        const data = await fs.readFile(path.resolve(__dirname, '../resources/data/template.dat'))
+        const data = await fs.readFile(path.resolve(__dirname, '../public/resources/data/template.dat'))
         const result = (await fs.readFile(path.resolve(__dirname, 'data/template.txt'))).toString()
         const decoded = dataDecrypter.decrypt(data)
         decoded.should.eqls(result)
     })
 
     it('data/weapon0.dat', async () => {
-        const data = await fs.readFile(path.resolve(__dirname, '../resources/data/weapon0.dat'))
+        const data = await fs.readFile(path.resolve(__dirname, '../public/resources/data/weapon0.dat'))
         const result = (await fs.readFile(path.resolve(__dirname, 'data/weapon0.txt'))).toString()
         const decoded = dataDecrypter.decrypt(data)
         decoded.should.eqls(result)
     })
 
     it('bg/sys/cuhk/bg.dat', async () => {
-        const data = await fs.readFile(path.resolve(__dirname, '../resources/bg/sys/cuhk/bg.dat'))
+        const data = await fs.readFile(path.resolve(__dirname, '../public/resources/bg/sys/cuhk/bg.dat'))
         const result = (await fs.readFile(path.resolve(__dirname, 'data/cuhk.txt'))).toString()
         const decoded = dataDecrypter.decrypt(data)
         decoded.should.eqls(result)
